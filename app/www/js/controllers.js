c3baed510e9afe8fa21f837a69d1da8825dafeaf
Your Name <mohamadib@gmail.com>
2016-04-12 02:02:58 -0700

    adding list of  buttons conatins slide images and exit and report

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -98,3 +98,90 @@ app.controller('SettingCtrl', function($scope) {
 
 });
 
+
+
+
+///////////////////////////////////////////
+app.controller('MainCtrl', ['$scope', '$ionicModal', '$ionicSlideBoxDelegate', function ($scope, $ionicModal, $ionicSlideBoxDelegate) {
+		
+  	$scope.aImages = [{
+      	'src' : 'img/suc1.png', 
+      	'msg' : 'החלק ימינה לאות עוד או תגע כדי לצאת'
+    	}, {
+        'src' : 'img/suc2.png', 
+        'msg' : ''
+      }, { 
+        'src' : 'img/suc4.png', 
+        'msg' : ''
+    }, { 
+        'src' : 'img/suc5.png', 
+        'msg' : ''
+    }, { 
+        'src' : 'img/suc6.png', 
+        'msg' : ''
+    }          
+                     ];
+  
+    
+    $scope.exit=function() {
+      
+        ionic.Platform.exitApp();
+    };
+    
+    $ionicModal.fromTemplateUrl('image-modal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+
+    $scope.openModal = function() {
+      $ionicSlideBoxDelegate.slide(0);
+      $scope.modal.show();
+    };
+    
+    $scope.openModal4 = function() {
+      $ionicSlideBoxDelegate.slide(0);
+       $scope.modal.show();
+    };
+
+    $scope.closeModal = function() {
+      $scope.modal.hide();
+    };
+
+    // Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function() {
+      $scope.modal.remove();
+    });
+    // Execute action on hide modal
+    $scope.$on('modal.hide', function() {
+      // Execute action
+    });
+    // Execute action on remove modal
+    $scope.$on('modal.removed', function() {
+      // Execute action
+    });
+    $scope.$on('modal.shown', function() {
+      console.log('Modal is shown!');
+    });
+
+    // Call this functions if you need to manually control the slides
+    $scope.next = function() {
+      $ionicSlideBoxDelegate.next();
+    };
+  
+    $scope.previous = function() {
+      $ionicSlideBoxDelegate.previous();
+    };
+  
+  	$scope.goToSlide = function(index) {
+      $scope.modal.show();
+      $ionicSlideBoxDelegate.slide(index);
+    }
+  
+    // Called each time the slide changes
+    $scope.slideChanged = function(index) {
+      $scope.slideIndex = index;
+    };
+  }
+]);
