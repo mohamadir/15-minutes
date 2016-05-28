242e98cfc0ccab74a3079178ead3930c1e073994
MUHAMMED IBRAHEM <mohamdib@gmail.com>
2016-05-28 18:39:42 +0300

    add title to more page

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -170,51 +170,7 @@ app.controller('ReportCtrl', function(
 
 // More Controller
 
-app.controller('MoreCtrl', function($cordovaInAppBrowser,$rootScope) {
-
-  var options = {
-      location: 'yes',
-      clearcache: 'yes',
-      toolbar: 'no'
-    };
-
-  document.addEventListener(function () {
-    $cordovaInAppBrowser.open('http://ngcordova.com', '_blank', options)
-      .then(function(event) {
-        // success
-      })
-      .catch(function(event) {
-        // error
-      });
-
-
-    $cordovaInAppBrowser.close();
-
-  }, false);
-
-  $rootScope.$on('$cordovaInAppBrowser:loadstart', function(e, event){
-
-  });
-
-  $rootScope.$on('$cordovaInAppBrowser:loadstop', function(e, event){
-    // insert CSS via code / file
-    $cordovaInAppBrowser.insertCSS({
-      code: 'body {background-color:blue;}'
-    });
-
-    // insert Javascript via code / file
-    $cordovaInAppBrowser.executeScript({
-      file: 'script.js'
-    });
-  });
-
-  $rootScope.$on('$cordovaInAppBrowser:loaderror', function(e, event){
-
-  });
-
-  $rootScope.$on('$cordovaInAppBrowser:exit', function(e, event){
-
-  });
+app.controller('MoreCtrl', function() {
 
 });
 
