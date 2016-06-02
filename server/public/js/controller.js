e7ccec4f837f48cbfffe38daf1bc9ed50c102be8
hadiab <abdalhadi.m92@gmail.com>
2016-06-02 23:04:47 +0300

    add form validation to ionic app

--- server/public/js/controller.js
+++ server/public/js/controller.js
@@ -5,6 +5,13 @@ mod.controller('HomeCtrl', function(){});
 mod.controller('ReportsCtrl', function($scope, Report){
 	console.log('> ReportsCtrl');
 	$scope.reports = Report;
+
+	$scope.exportData = function () {
+    var blob = new Blob([document.getElementById('reports-table').innerHTML], {
+        type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8"
+    });
+    saveAs(blob, "Reports.xls");
+  };
 });
 
 mod.controller('ReportDetailsCtrl', function($scope, $routeParams, Report){
